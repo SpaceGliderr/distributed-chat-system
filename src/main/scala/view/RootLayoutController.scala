ae593654faa3f9a12a1c4c73a7e5c432c28bd842
PisanoCohen <johnyan.yicheng@gmail.com>
2021-11-10 15:49:16 +0800

    Update RootLayout and its controller (subject to change in the future)

---
+++ src/main/scala/view/RootLayoutController.scala
@@ -0,0 +1,12 @@
+// import statements 
+// package view
+// import scalafxml.core.macros.sfxml
+// import scalafx.application.Platform
+// import MainApp
+
+// @sfxml
+// class RootLayoutController() {
+//     def handleClose() {
+//         Platform.exit()
+//     }
+// }
