e792bf4d2cbf84044ff1a0ed173739bfacdd25c3
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-12-09 18:35:37 +0800

    Add UserRoles enum to UserChatSession table

--- src/main/scala/model/UserChatSession.scala
+++ src/main/scala/model/UserChatSession.scala
@@ -4,11 +4,13 @@ import util.Database
 import scalikejdbc._
 import model.{ User, ChatSession }
 import java.util.UUID
+import util.UserRoles
 
 case class UserChatSession() extends Database {
     var id: String = ""
     var userId: List[User] = List()
     var chatSessionId: List[ChatSession] = List()
+    var role: UserRoles.UserRole = UserRoles.MEMBER
 
     def save(): Try[Long] = {
         Try (DB autoCommit { implicit session =>
@@ -26,6 +28,7 @@ object UserChatSession extends Database {
                     id int not null,
                     user_id int,
                     chat_session_id int,
+                    role varchar(64) check (role in ('ADMIN', 'MEMBER')),
                     foreign key (user_id) references users(id),
                     foreign key (chat_session_id) references chat_sessions(id),
                     primary key (id)
