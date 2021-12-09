4f13b38e8882cddcdf81f6304ddb639725693084
LimPeiNi <73421685+LimPeiNi@users.noreply.github.com>
2021-12-09 17:01:34 +0800

    login controller

--- src/main/scala/view/LogInController.scala
+++ src/main/scala/view/LogInController.scala
@@ -3,13 +3,34 @@ import scalafxml.core.macros.sfxml
 import scalafx.scene.control.TextField
 import scalafx.Includes._
 import chat.Main
+//import chat.model.User    <-- don't know if yall will change the directory for User class
 
 @sfxml
 class LogInController(
     private val phoneNumTextField: TextField,
     private val passwordTextField: TextField
 ){
-    def login(): Unit = println("") //*
+    var errorMessage = ""
+
+    def login(): Unit = {
+        if (phoneNumTextField.text().length == 0)
+            errorMessage += "No valid username\n"
+        if (passwordTextField.text().length == 0)
+            errorMessage += "No valid password"
+        else{
+            val tempUserList = User.users.toList
+            //see if yall want to change the User's username change to phone number or not
+            val index = tempUserList.indexWhere(user => user.phoneNum.value == phoneNumTextField.text.value)
+            if (index == -1)
+                //print error : no account
+            else{
+                if(User.users.apply(index).password.value == passwordTextField.text.value)
+                    //jump to chatroom page
+                else
+                    //print error : wrong password
+            }
+        }
+    }
 
     def cancel(): Unit = Main.showPages("view/Home.fxml")
 }
