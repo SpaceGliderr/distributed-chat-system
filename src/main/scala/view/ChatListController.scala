da9ba5d3488e25c6df23de0019181288ae20d283
LimPeiNi <73421685+LimPeiNi@users.noreply.github.com>
2021-12-10 00:50:04 +0800

    chat list

--- src/main/scala/view/ChatListController.scala
+++ src/main/scala/view/ChatListController.scala
@@ -8,10 +8,25 @@ import scalafx.scene.image.{ImageView, Image}
 class ChatListController(
     private val searchBar: TextField,
     private val imageView: ImageView,
-    private val listView: ListView[String]
+    private val imageView1: ImageView,
+    private val imageView2: ImageView,
+    private val imageView3: ImageView,
+    private val conversationList: ListView[String]
 ){
-    val image = new Image(getClass().getResourceAsStream("searchIcon.png"))
-    imageView.image_=(image)
+    val searchIcon = new Image(getClass().getResourceAsStream("searchIcon.png"))
+    imageView.image_=(searchIcon)
+    val newChatIcon = new Image(getClass().getResourceAsStream("newChatIcon.png"))
+    imageView1.image_=(newChatIcon)
+    val newGroupIcon = new Image(getClass().getResourceAsStream("newGroupIcon.png"))
+    imageView2.image_=(newGroupIcon)
+    val deleteIcon = new Image(getClass().getResourceAsStream("deleteIcon.png"))
+    imageView3.image_=(deleteIcon)
+
+
+    // 1.var conversations
+    // 2.var latestMsg
+    // 3.var combinedList
+    // 4.def updateConversationList()
 
     def search(): Unit = {
         if (!searchBar.visible.value){
@@ -19,8 +34,24 @@ class ChatListController(
             searchBar.requestFocus()
         }
         else{
-            //show result
+            // 5.filter conversation list
+
             searchBar.visible_=(false)
         }
+    }
+
+    def addNewChat: Unit = {
+        //try
+        println("add new chat")
+    }
+
+    def addNewGroup: Unit = {
+        //try
+        println("add new group")
+    }
+
+    def deleteConversation: Unit = {
+        //try
+        println("delete")
     }
 }
---
+++ src/main/scala/view/ChatRoomController.scala
@@ -0,0 +1,32 @@
+// package chat.view
+// import scalafxml.core.macros.sfxml
+// import scalafx.scene.control.{TextField, ListView}
+// import scalafx.Includes._
+// import scalafx.scene.image.{ImageView, Image}
+
+// @sfxml
+// class ChatRoomController(
+//     private val searchBar: TextField,
+//     private val imageView: ImageView,
+//     private val conversationList: ListView[String]
+// ){
+//     val image = new Image(getClass().getResourceAsStream("searchIcon.png"))
+//     imageView.image_=(image)
+
+//     // 1.var conversations
+//     // 2.var latestMsg
+//     // 3.var combinedList
+//     // 4.def updateConversationList()
+
+//     def search(): Unit = {
+//         if (!searchBar.visible.value){
+//             searchBar.visible_=(true)
+//             searchBar.requestFocus()
+//         }
+//         else{
+//             // 5.filter conversation list
+
+//             searchBar.visible_=(false)
+//         }
+//     }
+// }
