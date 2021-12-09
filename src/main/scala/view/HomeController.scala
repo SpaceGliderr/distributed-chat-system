96dfff0b50efc84032fcce79111a8cd08686cdff
LimPeiNi <73421685+LimPeiNi@users.noreply.github.com>
2021-12-09 15:45:22 +0800

    login
    login

--- src/main/scala/view/HomeController.scala
+++ src/main/scala/view/HomeController.scala
@@ -1,10 +1,19 @@
 package chat.view
+import java.io.FileInputStream
 import scalafxml.core.macros.sfxml
 import scalafx.Includes._
 import chat.Main
+import scalafx.scene.image.{ImageView, Image}
 
 @sfxml
-class HomeController(){
+class HomeController(
+    private val imageView: ImageView
+){
+    val image = new Image(getClass().getResourceAsStream("peopleOnDevices.png"))
+    imageView.image_=(image)
+    imageView.preserveRatio_=(false)
+    imageView.opacity_=(0.53)
+
     def login(): Unit = Main.showPages("view/LogIn.fxml")
 
     def signup(): Unit = Main.showPages("view/SignUp.fxml")
